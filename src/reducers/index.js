18ad4feb52446565e1646d32831495b46225df82
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-09-25 18:06:41 -0500

    create memes reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+
+function memes(state [], action) {
+  switch(action.type) {
+    case 'RECEIVE_MEMES':
+      return action.value;
+      break;
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({ memes });
+
+export default rootReducer;
