680ed0d835ca41f85dd142594b7cce81acd36612
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-09-30 16:54:45 -0500

    create new component to display UI for personal memes

---
+++ src/components/MyMemes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MyMemes extends Component {
+
+  render() {
+    return (
+      <div>
+        My Memes Component
+      </div>
+    );
+  }
+}
+
+export default MyMemes;
