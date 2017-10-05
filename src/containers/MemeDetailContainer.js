7f4c9d6c1306ce867f2d9b5b24e5f15871603ef6
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-04 23:34:48 -0500

    call mapDispatchToProps and pass in createMeme func

--- src/containers/MemeDetailContainer.js
+++ src/containers/MemeDetailContainer.js
@@ -2,4 +2,10 @@ import { connect } from 'react-redux';
 import MemeDetail from '../components/MemeDetail';
 import { createMeme } from '../actions';
 
+const mapDispatchToProps = dispatch => {
+  return {
+    createMeme: () => dispatch(createMeme()),
+  }
+}
+
 export default connect(null, { createMeme })(MemeDetail);
