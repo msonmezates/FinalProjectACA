a2404537d289e19d9544ffc8c465b9993599ce5f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-06 12:55:48 -0500

    switch App to class-based component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ListOfMemesContainer from '../containers/ListOfMemesContainer';
 
-function App() {
-  return (
-    <div>
-      <h2><u>Meme Generator</u></h2>
-      <ListOfMemesContainer />
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <h2><u>Meme Generator</u></h2>
+        <ListOfMemesContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
