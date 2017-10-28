66084630132330fd848a7429d995a11e3f634d71
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-28 11:48:51 -0500

    - Add mongoose - Name the database and connect through mongoose - Add all other dependencies

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/MemeGenerator');
 
 const app = express();
 
