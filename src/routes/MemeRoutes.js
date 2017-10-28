1e30d762d46cfef7f0d6cd245c9508feabf0c3c3
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-28 14:09:02 -0500

    Routes are created

--- src/routes/MemeRoutes.js
+++ src/routes/MemeRoutes.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/memes', list);
+router.get('/memes/:id', show);
+router.post('/memes', create);
+router.delete('/memes/:id', remove);
+
 module.exports = router;
